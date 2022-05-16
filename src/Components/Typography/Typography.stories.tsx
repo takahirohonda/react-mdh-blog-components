de2ec3b6adcf8fb174cfbb33021b5e42f75737b9
mydatahack <thon6thon6@hotmail.com>
2022-05-16 18:15:28 +1000

    docs(story): add quick brow fox

--- src/Components/Typography/Typography.stories.tsx
+++ src/Components/Typography/Typography.stories.tsx
@@ -5,6 +5,11 @@ import * as Typography from './Typography.styled'
 export default {
   title: 'Components/Typography',
   component: Typography,
+  docs: {
+    description: {
+      component: 'The quick brown fox jumps over the lazy dog.',
+    },
+  },
 }
 
 const TextRegularTemplate: Story = () => (
